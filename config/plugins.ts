d9f41960ed32e394df963a4834d746884f0c88d6
Prasad Chaparala <chaparala.prasad16@gmail.com>
2025-08-07 19:42:37 +0530

    feat: enable PostgreSQL upgrade in deployment workflow and add Google Cloud Storage configuration

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,3 +1,12 @@
 module.exports = ({ env }) => ({
-    // Empty for now - add GCS config once working
+    upload: {
+        config: {
+            provider: '@strapi-community/strapi-provider-upload-google-cloud-storage',
+            providerOptions: {
+                bucketName: env('GCS_BUCKET'),
+                publicFiles: true,
+                uniform: false,
+            },
+        },
+    },
 });
