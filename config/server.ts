025a9faf827d471cc1bee51064171f6bdbc6cdee
Prasad Chaparala <chaparala.prasad16@gmail.com>
2025-08-08 22:37:48 +0530

    feat: add custom domain support

--- config/server.ts
+++ config/server.ts
@@ -38,6 +38,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: env('PUBLIC_URL', 'https://cms.tharara.com'), // Add your custom domain
+  proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
   },
