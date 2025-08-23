bdfc12afcf302ff84c077d83ba33fcaec3223591
Prasad Chaparala <chaparala.prasad16@gmail.com>
2025-08-23 17:31:10 +0530

    Adding tharara bucket changes

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,11 +3,12 @@ module.exports = ({ env }) => ({
         config: {
             provider: '@strapi-community/strapi-provider-upload-google-cloud-storage',
             providerOptions: {
-                bucketName: env('GCS_BUCKET'),
+                bucketName: 'tharara-bucket', //env('GCS_BUCKET'),
                 publicFiles: true,
                 uniform: false,
                 // serviceAccount: env.json('GCS_SERVICE_ACCOUNT', {}),
-                baseUrl: `https://storage.googleapis.com/${env('GCS_BUCKET', 'tharara-bucket')}`,
+                // baseUrl: `https://storage.googleapis.com/${env('GCS_BUCKET', 'tharara-bucket')}`,
+                baseUrl: `https://storage.googleapis.com/tharara-bucket`,
                 basePath: '',
             },
         },
